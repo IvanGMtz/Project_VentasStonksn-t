9713ecfb895159692fc2d6b3d5e24d652f178a66
IvanGMtz <navi.garces24@gmail.com>
2023-07-25 15:23:38 -0500

    feat: :tada: Start project "VentasStonkn't"

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import dotenv from "dotenv";
+dotenv.config();
+const appExpress = express();
+
+appExpress.use(express.json());
+
+let config = JSON.parse(process.env.MY_CONFIG);
+appExpress.listen(config, ()=>{
+    console.log(`http://${config.hostname}:${config.port}`);
+});
