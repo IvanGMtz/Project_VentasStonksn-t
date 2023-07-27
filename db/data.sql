729f050830740e1b2f803874bd5c94f0f87e7515
IvanGMtz <navi.garces24@gmail.com>
2023-07-27 14:47:56 -0500

    feat: :card_file_box: Update data of database

--- db/data.sql
+++ db/data.sql
@@ -1,13 +1,13 @@
 USE databaseStonksnt;
 
-INSERT INTO Categoria (nombre, descripcion)
+INSERT INTO categoria (nombre, descripcion)
 VALUES
   ('Electrónica', 'Productos electrónicos'),
   ('Ropa', 'Prendas de vestir'),
   ('Alimentos', 'Productos comestibles'),
   ('Hogar', 'Artículos para el hogar');
 
-INSERT INTO Producto (nombre, precio, descripcion, categoria_id)
+INSERT INTO producto (nombre, precio, descripcion, categoria_id)
 VALUES
   ('Televisor', 499.99, 'Smart TV LED 50 pulgadas', 1),
   ('Laptop', 899.00, 'Laptop Intel Core i5, 8GB RAM, 512GB SSD', 1),
@@ -18,59 +18,59 @@ VALUES
   ('Cuchillo de Cocina', 12.50, 'Cuchillo de acero inoxidable', 4),
   ('Lámpara de Mesa', 29.99, 'Lámpara de mesa con base de madera', 4);
 
-INSERT INTO Empleado (nombre, puesto)
+INSERT INTO empleado (nombre, puesto)
 VALUES
   ('Juan Pérez', 'Vendedor'),
   ('Ana López', 'Gerente de Ventas'),
   ('Pedro Gómez', 'Asistente de Tienda'),
   ('María Ruiz', 'Vendedor');
 
-INSERT INTO Cliente (nombre, correo, telefono)
+INSERT INTO cliente (nombre, correo, telefono)
 VALUES
   ('Roberto Pérez', 'roberto@example.com', '+1234567890'),
   ('Laura Ramírez', 'laura.r@example.com', '+0987654321'),
   ('Carlos González', 'carlos.g@example.com', '+2345678901'),
   ('Sofía Martínez', 'sofia.m@example.com', '+3456789012');
 
-INSERT INTO ModoPago (nombre, descripcion)
+INSERT INTO modopago (nombre, descripcion)
 VALUES
   ('Tarjeta de Crédito', 'Pago con tarjeta de crédito'),
   ('Efectivo', 'Pago en efectivo'),
   ('Transferencia', 'Pago mediante transferencia bancaria');
 
-INSERT INTO Venta (fecha, empleado_id, cliente_id, modo_pago_id)
+INSERT INTO venta (fecha, empleado_id, cliente_id, modo_pago_id)
 VALUES
   ('2023-07-10', 1, 1, 1),
   ('2023-07-11', 2, 2, 2),
   ('2023-07-12', 3, 3, 1),
   ('2023-07-12', 1, 4, 3);
 
-INSERT INTO DetalleVenta (venta_id, producto_id, cantidad, precio_unitario)
+INSERT INTO detalleventa (venta_id, producto_id, cantidad, precio_unitario)
 VALUES
   (1, 1, 2, 499.99),
   (2, 2, 1, 899.00),
   (3, 3, 3, 19.99),
   (4, 4, 1, 39.99);
 
-INSERT INTO TipoPremio (nombre, descripcion)
+INSERT INTO tipopremio (nombre, descripcion)
 VALUES
   ('Económico', 'Premios con incentivos económicos'),
   ('No Económico', 'Premios sin incentivos económicos');
 
-INSERT INTO CategoriaPremio (nombre, descripcion)
+INSERT INTO categoriapremio (nombre, descripcion)
 VALUES
   ('Empleado del Mes', 'Premios otorgados al empleado del mes'),
   ('Aniversario Laboral', 'Premios otorgados en aniversarios laborales'),
   ('Trabajo en Equipo', 'Premios por destacado trabajo en equipo');
 
-INSERT INTO Premio (nombre, descripcion, tipo_premio_id, categoria_premio_id)
+INSERT INTO premio (nombre, descripcion, tipo_premio_id, categoria_premio_id)
 VALUES
   ('Bono de Ventas', 'Bono adicional por ventas', 1, 1),
   ('Día Libre Adicional', 'Día libre adicional por logros', 2, 1),
   ('Gift Card', 'Tarjeta regalo como reconocimiento', 1, 2),
   ('Reconocimiento Especial', 'Reconocimiento especial por desempeño', 2, 3);
 
-INSERT INTO EmpleadoPremio (empleado_id, premio_id, fecha)
+INSERT INTO empleadopremio (empleado_id, premio_id, fecha)
 VALUES
   (1, 1, '2023-07-10'),
   (2, 2, '2023-07-11'),
